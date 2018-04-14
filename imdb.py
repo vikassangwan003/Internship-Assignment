ab07365b193d9468a2018acb06f45d0fd785402a
vikassangwan003 <vikassangwan003@gmail.com>
2018-04-15 00:52:03 +0530

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -1,16 +1,11 @@
-import json
-data=json.load(open("imdb.json"))
-list=[]
-for i in range(len(data)):
-    #print(type(data[i]))
-    #cprint(data[i]["genre"])
-    if "Action" in data[i]["genre"]:
-        if(data[i]["imdb_score"]==8.8):
-            list.append(i)
-           
-for i in list:
-    print("Director:"+data[i]["director"])
-    print("Movie:"+data[i]["name"])
-    print("\n")
-                  
-        
+import pandas as pd
+
+df=pd.read_json("imdb.json")
+#print(df)
+df.to_csv('results.csv')
+df1=pd.read_csv('results.csv')
+#print(df1.head())
+df2=df1[df1['genre'].str.contains('Action') ]
+#df3=df2.sort_values('imdb_score',ascending=False)
+#print(df3.iloc[:3,[2,4,5]])
+print(df2.loc[df2.sort_values('imdb_score',ascending=False).index,['name','director']].head(3))
